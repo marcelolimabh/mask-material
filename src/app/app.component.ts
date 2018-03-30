12037965a942b271664792fc9dcb07655f8312ad
marcelolimabh <marcelolimabh@gmail.com>
2018-03-30 19:49:19 -0300

    Application Mask to Cpf and Phone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public maskForm: FormGroup;
+
   title = 'app';
+
+  constructor(private fb: FormBuilder){
+
+  }
+  ngOnInit(){
+
+    this.maskForm = this.fb.group({
+      cpf: ['03093584698'],
+      telefone: ['']
+    });
+
+  }
+
+  submit(){
+    let cpf = this.maskForm.controls['cpf'].value;
+    let telefone = this.maskForm.controls['telefone'].value;
+
+    console.log(cpf);
+    console.log(telefone);
+
+
+  }
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule, MatButtonModule, MatInputModule } from '@angular/material';
+import { NgxMaskModule } from 'ngx-mask'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    NgxMaskModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
